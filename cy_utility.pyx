a82d885e13b933f309312727d1670b98559c004e
Josh Greenwald <green@Joshs-MacBook-Pro.local>
2013-12-29 09:49:24 -0600

    assym kf

--- cy_utility.pyx
+++ cy_utility.pyx
@@ -27,3 +27,50 @@ def univariate_kf(np.ndarray[double, ndim=1] raw, double seed, double q, double 
             res[i] = xhatprev
         
     return res
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+def assymetrical_univariate_kf(np.ndarray[double, ndim=1] raw, double seed, double q, double r, double ratio=.5):
+    cdef:
+        long nr = len(raw)
+        double xhatprev,p=0,pprev=q,k=0,k2=0,change,last_valid
+        np.ndarray[DTYPE_t, ndim=2] res = np.zeros([nr,2], dtype=np.double) * np.NaN
+    xhatprev = seed
+    res[0] = seed
+    for i from 1 <= i < nr:
+        if not np.isnan(raw[i]):
+            change = raw[i] - xhatprev
+            res[i][0] = xhatprev + k*change
+            res[i][1] = xhatprev + k2*change
+            p = (1-k)*pprev
+            xhatprev = res[i]
+            pprev = p + q
+            k = pprev/(pprev+r)
+            k2 = pprev/(pprev+r*ratio)
+        else:
+            res[i][0] = xhatprev
+            res[i][1] = xhatprev
+        
+    return res    
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+def find_turns(np.ndarray[long,ndim=1] bids,np.ndarray[long,ndim=1] asks):
+    cdef:
+        long blen = bids.shape[0]
+        np.ndarray[long, ndim=1] res = np.zeros(blen, dtype=np.int64)
+        long prev_bid,prev_ask
+    prev_bid = bids[0]
+    prev_ask = asks[0]
+    for i from 1<= i < blen:
+        if not (bids[i]>0 and asks[i]>0):
+            continue
+        if bids[i]>=prev_ask:
+            res[i] = 1
+        elif asks[i]<=prev_bid:
+            res[i] = -1
+        else:
+            continue
+        prev_bid = bids[i]
+        prev_ask = asks[i]   
+    return res
