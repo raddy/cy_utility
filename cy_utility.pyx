cbf6de3c62fecc46bea9a17ad2935d58dc5d6130
Josh Greenwald <green@Joshs-MacBook-Pro.local>
2014-03-11 20:08:40 -0500

    fixed smart_kf

--- cy_utility.pyx
+++ cy_utility.pyx
@@ -57,7 +57,69 @@ def assymetrical_univariate_kf(np.ndarray[double, ndim=1] raw, double seed, doub
             res[i][0] = xhatprev
             res[i][1] = xhatprev
         
-    return res    
+    return res
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+def smart_kf(np.ndarray[double, ndim=1] raw, double seed, double q, double r, 
+    double ratio=.5,bias_avoidance=True,skeptic_multiplier=3):
+    cdef:
+        long nr = len(raw)
+        int bias = 0
+        double xhatprev,p=0,pprev=q,k=0,k2=0,change,last_valid
+        np.ndarray[DTYPE_t, ndim=2] res = np.zeros([nr,2], dtype=np.double) * np.NaN
+    xhatprev = seed
+
+    skeptic = univariate_kf(raw,seed,q*ratio**skeptic_multiplier,r)
+
+    res[0][0] = seed - q
+    res[0][1] = seed + q
+    biases = [0]
+    for i from 1 <= i < nr:
+        if not np.isnan(raw[i]):
+            change = raw[i] - xhatprev
+            effective_bias = 0
+            if change>0:
+                res[i][0] = xhatprev + k*change
+                if bias > 1:
+                    effective_bias = np.sqrt(bias) / 10.
+                elif bias<0:
+                    bias = int(bias / 1.5)
+
+                if not bias_avoidance:
+                    effective_bias = 0
+
+                if i>1000 and skeptic[i-1000]>raw[i]:
+                    res[i][1] = xhatprev + k2*change*(1.25+effective_bias)
+                else:
+                    res[i][1] = xhatprev + k2*change*(1+effective_bias)
+                xhatprev = res[i][0]
+                bias+=1
+            else:
+                res[i][1] = xhatprev + k*change
+                if bias< -1:
+                    effective_bias = np.sqrt(abs(bias)) / 10.
+                elif bias> 0:
+                    bias = int(bias / 1.5)
+
+                if not bias_avoidance:
+                    effective_bias = 0
+
+                if i> 1000 and skeptic[i-1000]<raw[i]:
+                    res[i][0] = xhatprev + k2*change*(1.25+effective_bias)
+                else:
+                    res[i][0] = xhatprev + k2*change*(1+effective_bias)
+                xhatprev = res[i][1]
+                bias -=1
+            p = (1-k)*pprev
+            pprev = p + q
+            k = pprev/(pprev+r)
+            k2 = pprev/(pprev+r*ratio)
+        else:
+            res[i][0] = xhatprev
+            res[i][1] = xhatprev
+        biases.append(bias)
+    return res,biases          
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
